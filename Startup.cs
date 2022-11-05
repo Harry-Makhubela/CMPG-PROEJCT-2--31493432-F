f7940f8a0a624ff2e9741c71f16049c7138e08e9
Harry Makhubela <31493432@student365.msfed.nwu.ac.za>
2022-11-05 19:54:46 +0200

    scaffolded all models

--- Startup.cs
+++ Startup.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
