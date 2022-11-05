9a37afc8398b6e1ac624379640603e4802093ac1
Harry Makhubela <31493432@student365.msfed.nwu.ac.za>
2022-11-05 20:04:43 +0200

    Created Authentication File Updated Appsettings

---
+++ Authentication/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿
+
+    using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore;
+
+namespace Project_2.Authentication
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+
+        public DbSet<Project_2.Models.Zone> Zone { get; set; }
+        public DbSet<Project_2.Models.Device> Device { get; set; }
+        public DbSet<Project_2.Models.Category> Category { get; set; }
+
+
+
+    }
+}
+
+
